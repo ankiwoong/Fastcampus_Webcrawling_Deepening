38ffdd0f6fd1dca7a272859f0b4a83fa57220de0
Ankiwoong <ankiwoong@gmail.com>
2020-08-22 21:46:19 +0900

    Scrapy 크롤링 실습 - result.jl

---
+++ section02_1/result.jl
@@ -0,0 +1,10 @@
+{"text": "Scrapy Cloud Secrets: Hub Crawl Frontier and How To Use It"}
+{"text": "Blog Comments API (BETA):\u00a0Extract Blog Comment DATA At Scale"}
+{"text": "Your Price Intelligence Questions Answered"}
+{"text": "Data Center Proxies vs. Residential Proxies"}
+{"text": "How to Get High Success Rates With Proxies: 3 Steps to Scale Up"}
+{"text": "Job Postings API: Stable release"}
+{"text": "Web Scraping Basics: A Developer\u2019s Guide To Reliably Extract Data"}
+{"text": "Extracting Article & News Data: The Importance of Data Quality"}
+{"text": "Price Gouging or Economics at Work: Price Intelligence to Track Consumer Sentiment"}
+{"text": "A Practical Guide to Web Data QA Part III: Holistic Data Validation Techniques"}
