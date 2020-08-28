92db7f36e58cc653392e41a33d17f9650ddd56d6
Ankiwoong <ankiwoong@gmail.com>
2020-08-28 12:03:23 +0900

    Scrapy Spider

--- section03_2/section03_2/spiders/class03_2.py
+++ section03_2/section03_2/spiders/class03_2.py
@@ -1,6 +1,10 @@
 from urllib import parse
 import scrapy
 
+# import logging
+
+# logger = logging.getLogger("Mylogger")
+
 # 스파이더 종류 : CrawlSpider, XMLFeedSpider, CSVFeedSpider, SitemapSpider
 class TestSpider(scrapy.Spider):
     name = "test4"
@@ -9,13 +13,26 @@ class TestSpider(scrapy.Spider):
     # 실행 방법 1
     # 멀티 도메인
     start_urls = [
-        "http://blog.scrapinghub.com/",
+        "https://blog.scrapinghub.com/",
         "https://www.naver.com",
         "https://www.daum.net",
     ]
 
+    # 사용자 시스템 설정(settings.py)
+    custom_settings = {"DOWNLOAD_DELAY": 2}
+
     def parse(self, response):
-        print(response)
+        # print(response)
+        # logger.info()
+        self.logger.info("Response URL : %s" % response.url)
+        self.logger.info("Response Status : %s" % response.status)
+
+        if response.url.find("scrapinghub"):
+            yield {"sitemap": response.url, "content": response.text[:100]}
+        elif response.url.find("naver"):
+            yield {"sitemap": response.url, "content": response.text[:100]}
+        else:
+            yield {"sitemap": response.url, "content": response.text[:100]}
 
     # 실행 방법 2
     # Request 각각 지정
