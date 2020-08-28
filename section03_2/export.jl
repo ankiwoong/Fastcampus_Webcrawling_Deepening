8b15cef65272b3aee17b103ef6fd7c470ba638ba
Ankiwoong <ankiwoong@gmail.com>
2020-08-28 12:03:33 +0900

    Scrapy Spider - export.jl

---
+++ section03_2/export.jl
@@ -0,0 +1,3 @@
+{"sitemap": "https://www.daum.net", "content": "<!DOCTYPE html>\n<html lang=\"ko\" class=\"\">\n<head>\n<meta charset=\"utf-8\"/>\n<title>Daum</title>\n<meta p"}
+{"sitemap": "https://www.naver.com", "content": "\n<!doctype html>                <html lang=\"ko\" data-dark=\"false\"> <head> <meta charset=\"utf-8\"> <ti"}
+{"sitemap": "https://blog.scrapinghub.com/", "content": "<!doctype html><!-- start coded_template: id:5871566911 path:generated_layouts/5871566907.html --><!"}
