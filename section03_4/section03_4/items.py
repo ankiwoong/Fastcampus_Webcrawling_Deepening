d4f96be898677cdc392c08a980b32222e20d526f
Ankiwoong <ankiwoong@gmail.com>
2020-09-07 06:58:09 +0900

    Scrapy Items - 2

--- section03_4/section03_4/items.py
+++ section03_4/section03_4/items.py
@@ -1,12 +1,16 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+# Scrapy Items
+# 장점
+# 1. 수집 데이터를 일관성 있게 관리 가능
+# 2. 데이터를 사전형(Dict)로 관리, 오타 방지
+# 3. 추후 가공 및 DB 저장 용이
 
 import scrapy
 
 
-class Section034Item(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ItArticle(scrapy.Item):
+    # 제목
+    title = scrapy.Field()
+    # 이미지
+    img_url = scrapy.Field()
+    # 본문 내용
+    contents = scrapy.Field()
